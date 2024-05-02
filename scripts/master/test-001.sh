cfcb0b85a349ff5ede0059545bcf3e0e397179d6
Pedro Toledo Correa <pedrotoledocorrea@gmail.com>
2024-05-01 23:23:14 -0400

    Improve folder handling

--- scripts/master/test-001.sh
+++ scripts/master/test-001.sh
@@ -1,4 +1,4 @@
-mkdir ~/data
+mkdir -p ~/data
 aws s3 cp s3://utfsm-datasets-inf356/vlt_observations/vlt_observations_000.csv ~/data/
-hdfs dfs -mkdir /data
+hdfs dfs -mkdir -p /data
 hdfs dfs -put ~/data/vlt_observations_000.csv /data/
