0208a8ad0c8aff84f6090ffaead56af14e82a689
Pedro Toledo Correa <pedrotoledocorrea@gmail.com>
2024-05-01 20:16:46 -0400

    Consolidating launch procedure

--- initialize.sh
+++
@@ -1,7 +0,0 @@
-source credentials/aws/credentials.sh
-IP_MASTER=`flintrock --config config/flintrock/config.yaml describe spark-cluster | grep master: | awk '{print $2}'`
-ssh -i keys/flintrock-sa-east-1.pem -o StrictHostKeyChecking=no ec2-user@$IP_MASTER 'ssh -T -o StrictHostKeyChecking=no ec2-user@$(hostname) "mkdir scripts"'
-scp -i keys/flintrock-sa-east-1.pem scripts/master/* ec2-user@$IP_MASTER:~/scripts
-ssh -i keys/flintrock-sa-east-1.pem ec2-user@$IP_MASTER 'chmod +x ~/scripts/*.sh'
-scp -i keys/flintrock-sa-east-1.pem config/hadoop/* ec2-user@$IP_MASTER:~/hadoop/conf/
-ssh -i keys/flintrock-sa-east-1.pem ec2-user@$IP_MASTER './hadoop/sbin/stop-dfs.sh && ./hadoop/sbin/start-dfs.sh'
--- launch.sh
+++ launch.sh
@@ -1,2 +1,27 @@
+#!/bin/bash
+set -e
 source credentials/aws/credentials.sh
+echo "### Launching cluster"
 flintrock --config config/flintrock/config.yaml launch spark-cluster
+IP_MASTER=`flintrock --config config/flintrock/config.yaml describe spark-cluster | grep master: | awk '{print $2}'`
+ssh -i keys/flintrock-sa-east-1.pem -o StrictHostKeyChecking=no ec2-user@$IP_MASTER 'ssh -o StrictHostKeyChecking=no ec2-user@$(hostname) "mkdir -p scripts"' 2>/dev/null
+echo "### Submit scripts"
+scp -i keys/flintrock-sa-east-1.pem scripts/master/* ec2-user@$IP_MASTER:~/scripts
+echo "### Configuring machines"
+scp -i keys/flintrock-sa-east-1.pem config/hadoop/* ec2-user@$IP_MASTER:~/hadoop/conf/
+ssh -i keys/flintrock-sa-east-1.pem -T ec2-user@$IP_MASTER << EOF
+    sudo yum -y -q update
+    echo 'export PATH=\$PATH:~/scripts' >> ~/.bashrc
+    chmod +x ~/scripts/*.sh
+    cp ~/hadoop/etc/hadoop/log4j.properties ~/hadoop/conf/
+    echo "### Configuring workers"
+    for slave in \$(cat ~/hadoop/conf/slaves); do
+        echo \$slave
+        ssh ec2-user@\$slave 'cp ~/hadoop/etc/hadoop/log4j.properties ~/hadoop/conf/' &
+        wait
+    done
+    echo "### Restarting hdfs"
+    ~/hadoop/sbin/stop-dfs.sh 2>&1 | grep -v 'WARNING'
+    ~/hadoop/sbin/start-dfs.sh 2>&1 | grep -v 'WARNING'
+    echo "### Ready"
+EOF
