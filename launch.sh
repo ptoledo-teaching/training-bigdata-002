53ee853640e7e5900f6f9f3587f675d6a02ce077
Pedro Toledo Correa <66681656+ptoledo-teaching@users.noreply.github.com>
2025-05-12 15:30:17 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,11 +4,11 @@ source credentials/aws/credentials.sh
 echo "### Launching cluster"
 flintrock --config config/flintrock/config.yaml launch spark-cluster
 IP_MASTER=`flintrock --config config/flintrock/config.yaml describe spark-cluster | grep master: | awk '{print $2}'`
-ssh -i credentials/keys/flintrock-sa-east-1.pem -o StrictHostKeyChecking=no ec2-user@$IP_MASTER 'ssh -o StrictHostKeyChecking=no ec2-user@$(hostname) "mkdir -p scripts"' 2>/dev/null
+ssh -i credentials/keys/flintrock-us-east-1.pem -o StrictHostKeyChecking=no ec2-user@$IP_MASTER 'ssh -o StrictHostKeyChecking=no ec2-user@$(hostname) "mkdir -p scripts"' 2>/dev/null
 echo "### Transfering files to master node"
-scp -i credentials/keys/flintrock-sa-east-1.pem scripts/* ec2-user@$IP_MASTER:~/scripts
-scp -i credentials/keys/flintrock-sa-east-1.pem config/spark/* ec2-user@$IP_MASTER:~/spark/conf/
-ssh -i credentials/keys/flintrock-sa-east-1.pem -T ec2-user@$IP_MASTER << EOF
+scp -i credentials/keys/flintrock-us-east-1.pem scripts/* ec2-user@$IP_MASTER:~/scripts
+scp -i credentials/keys/flintrock-us-east-1.pem config/spark/* ec2-user@$IP_MASTER:~/spark/conf/
+ssh -i credentials/keys/flintrock-us-east-1.pem -T ec2-user@$IP_MASTER << EOF
     echo "### Configuring master node"
     sudo yum -y -q update
     echo 'export PATH=\$PATH:~/scripts' >> ~/.bashrc
